59fcbb2b6114bfd7ac94872004d166a45b7bf159
Gijs ter Haar <gijs.terhaar@dataexpert.nl>
2025-07-29 14:56:12 +0200

    dictionary_search_1_1

--- dictionary_search.py
+++ dictionary_search.py
@@ -1,2 +1,4 @@
 def find_key(source_dictionary, target_string):
+    if target_string not in source_dictionary.keys():
+        return False
     return True
--- test_dictionary_search.py
+++ test_dictionary_search.py
@@ -13,3 +13,17 @@ def test_empty_dictionary():
     string_key = 'potato'
     result = find_key(dictionary, string_key)
     assert result is False
+
+
+def test_string_key_not_in_dictionary():
+    dictionary = {'beer': 'pint'}
+    string_key = 'potato'
+    result = find_key(dictionary, string_key)
+    assert result is False
+
+
+def test_dict_in__dict():
+    dictionary = {'1': {'beer': 'pint'}}
+    string_key = 'beer'
+    result = find_key(dictionary, string_key)
+    assert result is True
