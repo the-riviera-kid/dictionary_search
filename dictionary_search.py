ccefbeb34d899739c8cb75a28cc730c671223466
Dan Jackson <dan.jackson@dataexpert.nl>
2025-07-29 15:01:24 +0200

    Added test where the key matches the value, not the key

--- dictionary_search.py
+++ dictionary_search.py
@@ -1,4 +1,4 @@
 def find_key(source_dictionary, target_string):
-    if target_string not in source_dictionary.keys():
+    if target_string not in str(source_dictionary):
         return False
     return True
--- test_dictionary_search.py
+++ test_dictionary_search.py
@@ -21,9 +21,15 @@ def test_string_key_not_in_dictionary():
     result = find_key(dictionary, string_key)
     assert result is False
 
-
 def test_dict_in__dict():
     dictionary = {'1': {'beer': 'pint'}}
     string_key = 'beer'
     result = find_key(dictionary, string_key)
     assert result is True
+
+def test_dict_in_dict_value_match():
+    dictionary = {'1': {'beer': 'pint'}}
+    string_key = 'pint'
+    result = find_key(dictionary, string_key)
+    assert result is False
+
