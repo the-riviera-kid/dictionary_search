4a9a5a537c8a0d55ca17ebc607f6f3757a04e0fd
Dan Jackson <dan.jackson@dataexpert.nl>
2025-07-29 14:35:01 +0200

    Added test for empty dictionary

---
+++ dictionary_search.py
@@ -0,0 +1,2 @@
+def find_key(source_dictionary, target_string):
+    return True
--- test_dictionary_search.py
+++ test_dictionary_search.py
@@ -1,6 +1,15 @@
+from dictionary_search import find_key
+
+
 def test_find_dictionary():
     dictionary = {'beer': 'pint'}
     string_key = 'beer'
     result = find_key(dictionary, string_key)
     assert result is True
-    
+
+
+def test_empty_dictionary():
+    dictionary = {}
+    string_key = 'potato'
+    result = find_key(dictionary, string_key)
+    assert result is False
