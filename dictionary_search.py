cf3ff01cb3f1dbe2e8c146b0952b6aa9a1514630
Gijs ter Haar <gijs.terhaar@dataexpert.nl>
2025-07-29 17:25:08 +0200

    dictionary_search_1: added test to check for a key in an embedded dict

--- dictionary_search.py
+++ dictionary_search.py
@@ -1,4 +1,8 @@
 def find_key(source_dictionary, target_string):
     if target_string not in str(source_dictionary):
         return False
+    for i in source_dictionary.values():
+        if isinstance(i, dict):
+            if target_string not in str(i.keys()):
+                return False
     return True
